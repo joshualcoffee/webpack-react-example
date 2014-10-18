30c5812a25ba74c4e21055ed741f24bdc7ba2eec
Joshua Coffee <josh@joffeedesign.com>
2014-10-18 16:58:42 -0400

    no need for that loader

--- sample-module/webpack.config.js
+++ sample-module/webpack.config.js
@@ -8,8 +8,6 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
-      { test: /\.(png|jpg)$/, loader: 'jsx-loader'},
       { test: /\.js$/, loader: "jsx-loader" }
     ]
   }
